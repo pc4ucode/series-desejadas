bc21a18759a99539497a70674ae2243a02a2193c
euler <euler.amorim@hotmail.com>
2021-07-14 00:15:17 -0300

    continua aula 3 - 16:59

--- src/modules/auth/storage.js
+++ src/modules/auth/storage.js
@@ -2,5 +2,5 @@ import { setBearerToken } from '@/http'
 
 export const setHeaderToken = token => setBearerToken(token)
 export const getLocalToken = () => localStorage.getItem('token')
-export const deleteToken = () => localStorage.removeItem('token')
-export const setlocalToken = token => localStorage.setItem('token', token)
+export const deleteLocalToken = () => localStorage.removeItem('token')
+export const setLocalToken = token => localStorage.setItem('token', token)
--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -9,6 +9,35 @@ export const ActionDoLogin = ({ dispatch }, payload) => {
   })
 }
 
+export const ActionCheckToken = ({ dispatch, state }) => {
+  if (state.token) {
+    return Promise.resolve(state.token)
+  }
+
+  const token = storage.getLocalToken()
+
+  if (!token) {
+    return Promise.reject(new Error('Token inválido'))
+  }
+}
+
+export const ActionLoadSession = ({ dispatch }) => {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { token, user } = await services.auth.loadSession()
+
+      dispatch('ActionSetUser', user)
+      dispatch('ActionSetToken', token)
+
+      resolve()
+    } catch (e) {
+      dispatch('ActionSignOut')
+      reject(e)
+    }
+  })
+}
+
 export const ActionSetUser = ({ commit }, payload) => {
   commit(types.SET_USER, payload)
 }
@@ -17,4 +46,11 @@ export const ActionSetToken = ({ commit }, payload) => {
   storage.setHeaderToken(payload)
   storage.setlocalToken(payload)
   commit(types.SET_TOKEN, payload)
+}
+
+export const ActionSignOut = ({ dispatch }) => {
+  storage.setHeaderToken('')
+  storage.deleteLocalToken()
+  dispatch('ActionSetUser', {})
+  dispatch('ActionSetToken', '')
 }
